3737fafca45b592f5f107baa533b536e314c47c2
Skye McLean <skyemclean87@gmail.com>
2023-12-10 23:22:52 -0800

    Youtube Player, background and more projects

--- app/components/Projects.js
+++ app/components/Projects.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 export const Projects = () => {
   return (
     <div className="projects">
       Projects
       <div className="keychain">
-      <img id="keychain-1" style={{ objectFit: 'cover', width: '100vw', height: '20vh' }} src="/images/keychain.png" alt="Cars List 1" />
+      <img id="keychain-1" style={{ objectFit: 'cover', width: '100vw', height: '20vh' }} src="/images/keychain.png" alt="KeyChain App" />
+      <img id="keychain-1" style={{ objectFit: 'cover', width: '100vw', height: '20vh' }} src="/images/calculator.png" alt="Calculator App" />
+      <img id="keychain-1" style={{ objectFit: 'cover', width: '100vw', height: '20vh' }} src="/images/tetris.png" alt="Tetris App" />
+      <img id="keychain-1" style={{ objectFit: 'cover', width: '100vw', height: '20vh' }} src="/images/quotes.png" alt="Qotes App" />
+      <img id="keychain-1" style={{ objectFit: 'cover', width: '100vw', height: '20vh' }} src="/images/portfolio.png" alt="Portfolio App" />
+      <img id="keychain-1" style={{ objectFit: 'cover', width: '100vw', height: '20vh' }} src="/images/clock.png" alt="Clock App" />
+      <img id="keychain-1" style={{ objectFit: 'cover', width: '100vw', height: '20vh' }} src="/images/weather.png" alt="Weather App" />
+      <img id="keychain-1" style={{ objectFit: 'cover', width: '100vw', height: '20vh' }} src="/images/tictactoe.png" alt="TicTacToe App" />
       </div>
     </div>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import { Darkmode } from './components/darkmode.js';
 import { Introduction } from './components/Introduction.js';
 import { Skills } from './components/Skills.js';
 import { Projects } from './components/Projects.js'
+import YouTubePlayer from './components/YouTubePlayer';
 
 
 
@@ -21,14 +22,17 @@ export default function Home() {
       </div>
       <div className="bio">
         <Introduction />
+        <img id="coding-bg" src="/images/coding.png" alt="Coding BG" />
       </div>
       <div className="skills-div">
         <Skills />
       </div>
-      <div className="contact">
-      <button className="contact-btn" onClick={() => router.push('mailto:skyemclean87@gmail.com')}>Let's Chat</button>
+      <div className="youtube-player">
+        <YouTubePlayer videoId="vHq1ncfGqAk" />
       </div>
-  
+      <div className="contact">
+        <button className="contact-btn" onClick={() => router.push('mailto:skyemclean87@gmail.com')}>Let's Chat</button>
+      </div>
       <div className="main">
         <Projects />
       </div>
