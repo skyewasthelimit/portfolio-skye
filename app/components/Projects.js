4bfb385f0b0beee0673aff2e270bb9c170aedb96
Skye McLean <skyemclean87@gmail.com>
2023-12-14 14:00:18 -0800

    Updated Links for Projects

--- app/components/Projects.js
+++ app/components/Projects.js
@@ -5,16 +5,17 @@ export const Projects = () => {
     <div className="projects">
       Projects
       <div className="projects-wrapper">
-      <img id="keychain-1" style={{ objectFit: 'cover', width: '100vw', height: '20vh' }} src="/images/keychain.png" alt="KeyChain App" />
+      <a href="https://keychainapp.netlify.app" target="_blank"><img id="keychain-1" style={{ objectFit: 'cover', width: '100vw', height: '20vh' }} src="/images/keychain.png" alt="KeyChain App" /></a>
       <a href="https://skye-calculator.netlify.app" target="_blank"><img id="keychain-1" style={{ objectFit: 'cover', width: '100vw', height: '20vh' }} src="/images/calculator.png" alt="Calculator App" /></a>
       <a href="https://main--cute-blini-24ad79.netlify.app/" target="_blank"><img id="keychain-1" style={{ objectFit: 'cover', width: '100vw', height: '20vh' }} src="/images/tetris.png" alt="Tetris App" /></a>
       <a href="https://skye-quotes.netlify.app" target="_blank"><img id="keychain-1" style={{ objectFit: 'cover', width: '100vw', height: '20vh' }} src="/images/quotes.png" alt="Quotes App" /></a>
       <a href="https://skyewasthelimit.netlify.app" target="_blank"><img id="keychain-1" style={{ objectFit: 'cover', width: '100vw', height: '20vh' }} src="/images/portfolio.png" alt="Portfolio App" /></a>
       <a href="https://skye-clock.netlify.app" target="_blank"><img id="keychain-1" style={{ objectFit: 'cover', width: '100vw', height: '20vh' }} src="/images/clock.png" alt="Clock App" /></a>
-      <img id="keychain-1" style={{ objectFit: 'cover', width: '100vw', height: '20vh' }} src="/images/weather.png" alt="Weather App" />
+      <a href="https://skye-weatherapp.netlify.app" target="_blank"><img id="keychain-1" style={{ objectFit: 'cover', width: '100vw', height: '20vh' }} src="/images/weather.png" alt="Weather App" /></a>
       <a href="https://skye-tictactoe.netlify.app" target="_blank"><img id="keychain-1" style={{ objectFit: 'cover', width: '100vw', height: '20vh' }} src="/images/tictactoe.png" alt="TicTacToe App" /></a>
       </div>
     </div>
   )
 }
+
 
